a5c9baa5ba4d19967364aac8bffa0aeed90bd081
unknown <carson.ryoung@gmail.com>
2026-09-18 21:52:54 -0600

    Completed Core requirement 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,29 +6,39 @@ class Program
     {
         Console.Write("What is your grade percentage? ");
         string valueInText = Console.ReadLine();
-        
+
         // Converts the string to an integer
         int percentage = int.Parse(valueInText);
 
         if (percentage >= 90)
         {
-            Console.Write("You received an A");
+            Console.WriteLine("You received an A");
         }
         else if (percentage >= 80)
         {
-            Console.Write("You received a B");
+            Console.WriteLine("You received a B");
         }
         else if (percentage >= 70)
         {
-            Console.Write("You received a C");
+            Console.WriteLine("You received a C");
         }
         else if (percentage >= 60)
         {
-            Console.Write("You received a D");
+            Console.WriteLine("You received a D");
         }
         else
         {
-            Console.Write("You received an F");
+            Console.WriteLine("You received an F");
+        }
+
+        // if else statements to determine if course was passed
+        if (percentage >= 70)
+        {
+            Console.Write("You passed the class. Congratulations!");
+        }
+        else
+        {
+            Console.Write("You did not pass the class. Try again, you can do this!");
         }
     }
 }
