458c9014c543c20a2a298ed134a8ae735e42cd47
unknown <carson.ryoung@gmail.com>
2026-09-18 21:42:07 -0600

    Met core requirement one

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueInText = Console.ReadLine();
+        
+        // Converts the string to an integer
+        int percentage = int.Parse(valueInText);
+
+        if (percentage >= 90)
+        {
+            Console.Write("You received an A");
+        }
+        else if (percentage >= 80)
+        {
+            Console.Write("You received a B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.Write("You received a C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.Write("You received a D");
+        }
+        else
+        {
+            Console.Write("You received an F");
+        }
     }
 }
