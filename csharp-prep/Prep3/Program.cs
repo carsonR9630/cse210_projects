05f4169b731ef1af6ddaddc548d39ee6e39cc950
unknown <carson.ryoung@gmail.com>
2026-09-25 15:58:03 -0600

    Completed Core requirement 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string userMagicNumber = "";
+        string userGuessNumber = "";
+        // get magic number
+        Console.Write("What is the magic number? ");
+        userMagicNumber = Console.ReadLine();
+
+        // get user's guess
+        Console.Write("What is your guess? ");
+        userGuessNumber = Console.ReadLine();
+
+        // convert from string to integer
+        int magicNumber = int.Parse(userMagicNumber);
+        int guessNumber = int.Parse(userGuessNumber);
+
+        // determine if its higher, lower, or equal
+        if (magicNumber > guessNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (magicNumber < guessNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
