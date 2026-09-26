251ef84967e6ba1710c263ac3cd7e404d6687338
unknown <carson.ryoung@gmail.com>
2026-09-25 20:24:50 -0600

    Completed Core Requirement 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,27 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        string userMagicNumber = "";
+        // declaring variables
         string userGuessNumber = "";
-        // get magic number
-        Console.Write("What is the magic number? ");
-        userMagicNumber = Console.ReadLine();
-
-        // convert from string to integer
-        int magicNumber = int.Parse(userMagicNumber);
+        // declare guessNumber so it exists outside the do
         int guessNumber = 0;
+
+        // get magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        Console.WriteLine("The magic number is between 1 and 100.");
 
         do
         {
             // get user's guess
             Console.Write("What is your guess? ");
-            userGuessNumber = Console.ReadLine();     
+            userGuessNumber = Console.ReadLine();
+            // convert from string to integer     
             guessNumber = int.Parse(userGuessNumber);
+            // figure out if it's higher or lower
             if (magicNumber > guessNumber)
             {
                 Console.WriteLine("Higher");
             }
-            else if ( magicNumber < guessNumber)
+            else if (magicNumber < guessNumber)
             {
                 Console.WriteLine("Lower");
             }
