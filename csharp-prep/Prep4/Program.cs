16048363d6cc62dc4747f8b5dbbccba77421004d
unknown <carson.ryoung@gmail.com>
2026-09-25 21:38:45 -0600

    Completed Core Requirement 2

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -11,6 +11,7 @@ class Program
         // Instructions for user
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
+        // Loop to get numbers in list
         do
         {
             Console.Write("Enter Number: ");
@@ -22,11 +23,17 @@ class Program
             }
         } while(userNumber != 0);
 
+        // get the sum
         int sum = 0;
         foreach (int number in numbers)
         {
             sum += number;
         }
         Console.WriteLine($"The sum is: {sum}");
+
+        // get the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
     }
 }
