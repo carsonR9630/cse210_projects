1af0797a5bdd4cadda8befad5d2a4d185736ea76
unknown <carson.ryoung@gmail.com>
2026-09-25 21:23:01 -0600

    Completed core Requirement 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Create list
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        // Instructions for user
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter Number: ");
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while(userNumber != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
